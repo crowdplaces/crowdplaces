5bc529f94d0a652e9477ba3e35117858a08bb6fb
Daniel Weinmann <danielweinmann@gmail.com>
2012-05-29 21:49:26 -0300

    Adding stencil and changing locales

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -78,7 +78,7 @@ class ApplicationController < ActionController::Base
     new_locale = current_user.locale if current_user
     new_locale = session[:locale] if session[:locale]
     unless new_locale
-      new_locale = request.compatible_language_from(I18n.available_locales.map(&:to_s))
+      new_locale = request.compatible_language_from(I18n.available_locales.select{ |l| l != :pt }.map(&:to_s))
       new_locale = I18n.default_locale.to_s unless new_locale
       flash[:locale] = t('notify_locale', :locale => new_locale)
     end
--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -6,4 +6,4 @@ I18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 I18n.available_locales = Dir[Rails.root.join('config', 'locales', '*.{yml}')].collect {|l| locale = l.scan(/.+\/(..)\.yml/)[0]; locale[0].to_sym unless locale.nil?}.delete_if {|locale| locale.nil?}
 
 # set default locale to something other than :en
-I18n.default_locale = :pt
+I18n.default_locale = :es
