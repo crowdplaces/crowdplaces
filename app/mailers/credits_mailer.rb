808ced5130dc03703dc9e90663728b9df9ff396f
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-07-17 15:56:11 -0300

    removed hardcoded email infos into credits_mailer

--- app/mailers/credits_mailer.rb
+++ app/mailers/credits_mailer.rb
@@ -1,10 +1,9 @@
 class CreditsMailer < ActionMailer::Base
   include ERB::Util
-  default :from => "Crowdplaces <system@crowdplaces.org>"
 
   def request_refund_from(backer)
     @backer = backer
     @user = backer.user
-    mail(:to => 'payments@crowdplaces.org', :subject => I18n.t('credits_mailer.request_refund_from.subject', :name => @user.name))
+    mail(:from => "#{I18n.t('site.name')} <#{I18n.t('site.email.system')}>", :to => I18n.t('site.email.payments'), :subject => I18n.t('credits_mailer.request_refund_from.subject', :name => @user.name))
   end
 end
