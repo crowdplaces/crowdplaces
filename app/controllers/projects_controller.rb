091753e8117e925f49399029146d5b6627f055bf
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-15 14:28:50 -0300

    changing the curated pages logo size

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -46,7 +46,7 @@ class ProjectsController < ApplicationController
         #@events = Rails.cache.fetch 'calendar', expires_in: 30.minutes do
           #calendar.fetch_events_from("catarse.me_237l973l57ir0v6279rhrr1qs0@group.calendar.google.com") || []
         #end
-        @curated_pages = CuratedPage.visible.order("created_at desc").limit(12)
+        @curated_pages = CuratedPage.visible.order("created_at desc").limit(18)
         @last_tweets = Rails.cache.fetch('last_tweets', :expires_in => 30.minutes) do
           begin
             JSON.parse(Net::HTTP.get(URI("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=#{t('site.twitter')}")))[0..5]
