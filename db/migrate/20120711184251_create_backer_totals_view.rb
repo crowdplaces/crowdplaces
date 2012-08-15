92107ea56b5878cdcdfd4f501b6fc2f48dfc796c
Josemar Davi Luedke <josemarluedke@gmail.com>
2012-08-15 13:56:03 -0300

    adjusting something

--- db/migrate/20120711184251_create_backer_totals_view.rb
+++ db/migrate/20120711184251_create_backer_totals_view.rb
@@ -1,7 +1,7 @@
 class CreateBackerTotalsView < ActiveRecord::Migration
   def up
     execute <<SQL
-    CREATE OR REPLACE VIEW backer_totals AS 
+    CREATE OR REPLACE VIEW backer_totals AS
     SELECT project_id, sum(value) AS pledged, count(*) AS total_backers
     FROM backers
     WHERE confirmed = true
